2cbfcc0c61d3d0335579e6918d65c4fd3f1cd455
cmhedrick <christopher.m.hedrick@gmail.com>
2017-06-01 21:43:00 -0400

    day 1/100 init commit. basic UI for Xam App

---
+++ DummyDB/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace DummyDB
+{
+    [Activity(Label = "DummyDB", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
